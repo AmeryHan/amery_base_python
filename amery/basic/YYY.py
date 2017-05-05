464dd7e4f9ce3ec801bfd9024eb36816cf5e455a
Amery Han <ahan@coupang.com>
2017-05-05 10:44:11 +0800

    add some test cases

--- amery/basic/YYY.py
+++ amery/basic/YYY.py
@@ -1,2 +1,23 @@
+# -*- coding: UTF-8 -*-
 
 
+# 导入模块
+import support
+
+# 现在可以调用模块里包含的函数了
+support.print_func("Runoob")
+
+import re
+print(re.match('www', 'www.runoob.com').span())  # 在起始位置匹配
+print(re.match('com', 'www.runoob.com'))         # 不在起始位置匹配
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match(r'(.*) are (.*?) .*', line, re.M | re.I)
+
+if matchObj:
+    print "matchObj.group() : ", matchObj.group()
+    print "matchObj.group(1) : ", matchObj.group(1)
+    print "matchObj.group(2) : ", matchObj.group(2)
+else:
+    print "No match!!"
--- amery/basic/indent.py
+++ amery/basic/indent.py
@@ -135,3 +135,37 @@ print "6 - c 的值为：", c
 
 c //= a
 print "7 - c 的值为：", c
+
+
+# 可写函数说明
+def printinfo(arg1, *vartuple):
+    "打印任何传入的参数"
+    print "输出: "
+    print arg1
+    for var in vartuple:
+        print var
+    return;
+
+
+# 调用printinfo 函数
+printinfo(10);
+printinfo(70, 60, 50);
+
+sum = lambda arg1, arg2: arg1 + arg2
+
+print "sum = ", sum(10, 20)
+
+total = 0;  # 这是一个全局变量
+
+
+# 可写函数说明
+def sum(arg1, arg2):
+    # 返回2个参数的和."
+    total = arg1 + arg2;  # total在这里是局部变量.
+    print "函数内是局部变量 : ", total
+    return total;
+
+
+# 调用sum函数
+sum(10, 20);
+print "函数外是全局变量 : ", total
